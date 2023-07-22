9afc5d46ebf0b3222baca27bed137b252e7654d4
lopez <patrice.lopez@science-miner.com>
2023-07-22 21:36:42 +0200

    review and extend the process

--- screen_it_pipeline.py
+++ screen_it_pipeline.py
@@ -1,4 +1,5 @@
 from article_dataset_builder.harvest import Harverster
+from article_dataset_builder.nlm2tei import Nlm2tei
 from software_mentions_client.client import software_mentions_client
 import requests
 import tqdm
@@ -10,4 +11,10 @@ version('lmdb')
 
 harvester = Harverster(config_path="config/config-harvester.json", dump_metadata=True)
 harvester.harvest_pmcids("config/comparison_full_set-pmcids.csv")
+
+nlm2tei = Nlm2tei(config_path="config/config-harvester.json")
+nlm2tei.process()
+
+client = software_mentions_client(config_path="config/config-client.json")
+client.annotate_collection("./data", force=True)
 
