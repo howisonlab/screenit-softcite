2f23ffa73f4fb8af87e45c75366d8ea9c459b5d8
James Howison <james@howison.name>
2023-07-24 16:25:38 -0500

    Added docker-compose for server and adjusted devcontainer folder.

--- screen_it_pipeline.py
+++ screen_it_pipeline.py
@@ -9,16 +9,21 @@ version('article_dataset_builder')
 version('software_mentions_client')
 version('lmdb')
 
+# Patrice pointed out the input file is pcmids not DOIs.
+#https://github.com/howisonlab/screenit-softcite/issues/1
+# First convert file so that it is one pmcid per line.
+# !cut -d ',' -f1 config/comparison_full_set.csv | tail -n +2 > config/comparison_full_set-pmcids.csv
+
 harvester = Harverster(config_path="config/config-harvester.json", dump_metadata=True)
-harvester.harvest_pmcids("config/comparison_full_set-pmcids.csv")
+# harvester.harvest_pmcids("config/comparison_full_set-pmcids.csv")
 
 # Not clear to me if the resulting tei.xml files are used by the client?
 # Running it is not idempotent, seems to repeat even if the tei.xml files are there?
 nlm2tei = Nlm2tei(config_path="config/config-harvester.json")
-nlm2tei.process()
+# nlm2tei.process()
 
 client = software_mentions_client(config_path="config/config-client.json")
-client.annotate_collection("./data")
+client.annotate_collection("./data", force=True)
 
 harvester.diagnostic(full=True)
 
